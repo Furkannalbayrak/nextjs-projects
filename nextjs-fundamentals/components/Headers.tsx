bacaedc0e54364d4f233c499bafe6e3ec8f8a92f
Furkan Albayrak <furkanalb42@gmail.com>
2025-11-30 18:47:03 +0300

    sheet componenti Header.tsx dosyasına eklendi

--- nextjs-fundamentals/components/Headers.tsx
+++ nextjs-fundamentals/components/Headers.tsx
@@ -1,18 +1,21 @@
 import Link from 'next/link'
-import React from 'react'
 import { ModeToggle } from './ModeToggle'
+import { SheetDemo } from './SheetDemo'
 
 function Headers() {
     return (
         <div>
             <div className='bg-blue-400 dark:bg-gray-700'>
-                <div className='container mx-auto flex items-center justify-between px-4 py-3'>
+                <div className='max-w-6xl mx-auto flex items-center justify-between md:px-10 px-5 md:py-3 py-4'>
                     <Link className='text-xl font-poppins text-white' href={'/'}>NextMovies 🎬</Link>
-                    <nav className='flex sm:gap-10 gap-4 items-center'>
+                    <nav className='hidden md:flex sm:gap-8 gap-4 items-center'>
                         <Link className='text-white font-bold' href={'https://github.com/Furkannalbayrak'}>GitHub</Link>
                         <Link className='text-white font-bold' href={'https://www.linkedin.com/in/furkanalbayrakdev/'}>LinkedIn</Link>
-                        <ModeToggle/>
+                        <ModeToggle />
                     </nav>
+                    <div className='flex md:hidden'>
+                        <SheetDemo/>
+                    </div>
                 </div>
             </div>
         </div>
